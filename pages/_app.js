5a6c64a0512530192c4e71f34f2266f31ac5a6af
omer-ayhan <om.ayhan247@gmail.com>
2022-06-30 16:41:28 +0300

    feat: deployment

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import Layout from "layout/Layout";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>Sola Store</title>
+			</Head>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
